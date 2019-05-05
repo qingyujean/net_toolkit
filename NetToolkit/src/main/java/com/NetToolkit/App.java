816476713bc0e9da878fd8df7cd1650b11314369
qingyujean <995168634@qq.com>
2019-05-05 13:38:17 +0800

    封装的网络测试工具集

---
+++ NetToolkit/src/main/java/com/NetToolkit/App.java
@@ -0,0 +1,13 @@
+package com.NetToolkit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
